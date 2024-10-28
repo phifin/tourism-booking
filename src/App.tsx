115e313f1f6033249e5d79598dfcf07e3316e47b
Vo Nhu Phi <nhuphivo@gmail.com>
2024-10-28 10:08:43 +0700

    config css

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <p className='read-the-docs bg-red-500'>Click on the Vite and React logos to learn more</p>
     </>
   )
 }
